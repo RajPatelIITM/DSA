b11a6289a289ad8d5dec331e77774fb601aa5110
RajPatelIITM <rajpatel231103@gmail.com>
2024-09-29 14:18:28 +0530

    Level 3 Done

--- Basic C++ Codes/07_pattern_level3.cpp
+++ Basic C++ Codes/07_pattern_level3.cpp
@@ -3,5 +3,74 @@ using namespace std;
 
 int main(){
     //17th pattern example
-    
+    // int n;
+    // cin >> n;
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         cout << "  ";
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 18th example
+    // int n;
+    // cin >> n;
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         cout << "  ";
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 19th example
+    // int n;
+    // cin >> n;
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         cout << "  ";
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    //20th example
+    // int n;
+    // cin >> n;
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n-i;j++){
+    //         cout << "  ";
+    //     }
+    //     char ch='A';
+    //     for(int j=1;j<=i;j++){
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // 21st pattern example
+    int n;
+    cin >> n;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout << "  ";
+        }
+        for(int j=i;j>=1;j--){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
 }
---
+++ Basic C++ Codes/08_pattern_level4.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // 22nd pattern example
+}
