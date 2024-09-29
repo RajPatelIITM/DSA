b308bfa43e89a9acec70e376e416256e80a38a35
RajPatelIITM <rajpatel231103@gmail.com>
2024-09-29 12:54:21 +0530

    Level 1 Done

---
+++ Basic C++ Codes/05_pattern_level1.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // 1st Pattern Example
+    
+    // for(int i=1; i <= 5; i++){
+    //     for(int j=1;j<=5;j++){
+    //         cout << "* "; 
+    //     }
+    //     cout << endl;
+    // }
+
+    // Note: I will be row and J will be column.
+
+    // 2nd Pattern Example
+
+    // for(int i=1; i <= 4; i++){
+    //     for(int j=1; j <= 5; j++){
+    //         cout << "10 ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 3rd Pattern Example
+
+    // for(int i = 1; i <= 5; i++){
+    //     for(int j = 1; j <= 5; j++){
+    //         cout << i << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 4th Pattern example
+
+    // for(int i=1;i<=5;i++){
+    //     for(int j=1;j<=5;j++){
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 5th pattern example
+
+    // for(int i=1;i<=5;i++){
+    //     for(int j=5;j>=1;j--){
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    //6th pattern example
+
+    // for(char ch='a';ch <= 'e';ch++){
+    //     for(int j=1;j<=5;j++){
+    //         cout << ch << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    //7th pattern example
+
+    // for(int i=1;i<=5;i++){
+    //     for(char ch='a';ch<='e';ch++){
+    //         cout << ch << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // 8th example
+
+    // 8.1 with extra variable
+    // int count = 1;
+    // for(int i=1;i<=5;i++){
+    //     for(int j=1;j<=5;j++){
+    //         cout << count << " ";
+    //         count++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // 8.2 without extra variable
+    // for(int i=1;i<=5;i++){
+    //     for(int j=1;j<=5;j++){
+    //         cout << (i-1)*5 + j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+}
