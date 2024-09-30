381e07642b9aebb18b516e9dc50f2bb550a2574e
RajPatelIITM <rajpatel231103@gmail.com>
2024-09-30 11:43:01 +0530

    while do-while switch done

---
+++ Basic C++ Codes/09_while.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // Example1 : Prinitng n to 1
+    // int n;
+
+    // cout << "Enter the Number: ";
+    // cin >> n;
+
+    // while(n>=1){
+    //     cout << n << " ";
+    //     n--;
+    // }
+    // cout << "\nHappy Learning";
+
+    // Example 2 Factors of a Number
+    // int n;
+
+    // cout << "Enter the Number: ";
+    // cin >> n;
+    // int i=1;
+    // while(i<=n){
+    //     if(n%i==0){
+    //         cout << i << " ";
+    //     }
+    //     i++;
+    // }
+    // cout << "\nHappy Learning";
+    
+}
